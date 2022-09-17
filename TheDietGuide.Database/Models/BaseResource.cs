9107c11646851b5a1fad11918ff792b993dbc60e
Christopher Plummer <chrisplummer0194@gmail.com>
2022-09-16 23:41:56 -0400

    Added new DB class library and base classes

---
+++ TheDietGuide.Database/Models/BaseResource.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TheDietGuide.Database.Models;
+
+public abstract class BaseResource
+{
+    [Required] [BsonId] public string Id { get; set; }
+    [Required] public DateTime CreatedAt { get; set; }
+    [Required] public DateTime UpdatedAt { get; set; }
+}
---
+++ TheDietGuide.Database/Repositories/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using TheDietGuide.Database.Models;
+
+namespace TheDietGuide.Database.Repositories;
+
+public abstract class BaseRepository<T> where T : BaseResource
+{
+    private readonly IMongoClient _mongoClient;
+    protected string CollectionName;
+
+    protected BaseRepository(IMongoClient mongoClient)
+    {
+        _mongoClient = mongoClient;
+    }
+
+    public virtual async Task<List<T>> GetAll()
+    {
+        var items = await GetCollection().AsQueryable().ToListAsync();
+        return items;
+    }
+
+    public virtual async Task<T> GetById(string id)
+    {
+        var item = await GetCollection().AsQueryable()
+            .FirstOrDefaultAsync(w => w.Id.Equals(id));
+        return item;
+    }
+    
+    public virtual async Task<T> Create(T data)
+    {
+        data.Id = Guid.NewGuid().ToString();
+        data.CreatedAt = DateTime.UtcNow;
+        data.UpdatedAt = data.CreatedAt;
+        await GetCollection().InsertOneAsync(data);
+        var items = await GetCollection().AsQueryable().ToListAsync();
+        return items.FirstOrDefault(x => x.Id.Equals(data.Id));
+    }
+
+    public virtual async Task<T> Update(string id, T data)
+    {
+        data.UpdatedAt = DateTime.UtcNow;
+        await GetCollection().ReplaceOneAsync(x => x.Id.Equals(id), data);
+        return data;
+    }
+
+    public virtual Task Delete(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected IMongoCollection<T> GetCollection()
+    {
+        var database = _mongoClient.GetDatabase("");
+        var collection = database.GetCollection<T>(CollectionName);
+        return collection;
+    }
+}
