f13cda3116f1cc292c19336e6b2076380e83917b
Jordan Garcia <jordan.garcia.roche@gmail.com>
2022-09-18 23:12:29 -0400

    Jg tdg 3 (#4)
    * Create RecipeInformation
    
    * Create RecipeIngredients
    
    * updated to RecipeMetric
    
    * Create RecipeNutrition
    
    * Create RecipeSearchResults

---
+++ TheDietGuide.ThirdParty/APIs/Spoonacular/Models/RecipeIngredients.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheDietGuide.ThirdParty.APIs.Spoonacular.Models;
+
+public class RecipeIngredients
+{
+    [JsonPropertyName("ingredients")]
+    public List<Ingredient> Ingredients { get; set; }  
+}
+
+// Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+public class IngredientAmount
+{
+    [JsonPropertyName("metric")]
+    public IngredientMetric Metric { get; set; }
+
+    [JsonPropertyName("us")]
+    public IngredientUs Us { get; set; }
+}
+
+public class Ingredient
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("image")]
+    public string Image { get; set; }
+
+    [JsonPropertyName("amount")]
+    public IngredientAmount Amount { get; set; }
+}
+
+public class IngredientMetric
+{
+    [JsonPropertyName("value")]
+    public double Value { get; set; }
+
+    [JsonPropertyName("unit")]
+    public string Unit { get; set; }
+}
+
+public class IngredientUs
+{
+    [JsonPropertyName("value")]
+    public double Value { get; set; }
+
+    [JsonPropertyName("unit")]
+    public string Unit { get; set; }
+}
+
---
+++ TheDietGuide.ThirdParty/APIs/Spoonacular/Models/RecipeSearchResults.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheDietGuide.ThirdParty.APIs.Spoonacular.Models;
+
+public class RecipeSearchResults
+{
+    [JsonPropertyName("results")]
+    public List<Result> Results { get; set; }
+
+    [JsonPropertyName("offset")]
+    public int Offset { get; set; }
+
+    [JsonPropertyName("number")]
+    public int Number { get; set; }
+
+    [JsonPropertyName("totalResults")]
+    public int TotalResults { get; set; }
+}
+
+// Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+public class Result
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("image")]
+    public string Image { get; set; }
+
+    [JsonPropertyName("imageType")]
+    public string ImageType { get; set; }
+}
+
