8b0c73be750ed48245feeb6ab32d4904879221a1
Jordan Garcia <jordan.garcia.roche@gmail.com>
2022-09-18 23:06:42 -0400

    Create RecipeNutrition

---
+++ TheDietGuide.ThirdParty/APIs/Spoonacular/Models/RecipeNutrition.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheDietGuide.ThirdParty.APIs.Spoonacular.Models;
+
+public class RecipeNutrition
+{
+    [JsonPropertyName("calories")]
+    public string Calories { get; set; }
+
+    [JsonPropertyName("carbs")]
+    public string Carbs { get; set; }
+
+    [JsonPropertyName("fat")]
+    public string Fat { get; set; }
+
+    [JsonPropertyName("protein")]
+    public string Protein { get; set; }
+
+    [JsonPropertyName("bad")]
+    public List<Bad> Bad { get; set; }
+
+    [JsonPropertyName("good")]
+    public List<Good> Good { get; set; }
+
+    [JsonPropertyName("expires")]
+    public long Expires { get; set; }
+
+    [JsonPropertyName("isStale")]
+    public bool IsStale { get; set; }
+}
+
+// Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+public class Bad
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("amount")]
+    public string Amount { get; set; }
+
+    [JsonPropertyName("indented")]
+    public bool Indented { get; set; }
+
+    [JsonPropertyName("percentOfDailyNeeds")]
+    public double PercentOfDailyNeeds { get; set; }
+}
+
+public class Good
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("amount")]
+    public string Amount { get; set; }
+
+    [JsonPropertyName("indented")]
+    public bool Indented { get; set; }
+
+    [JsonPropertyName("percentOfDailyNeeds")]
+    public double PercentOfDailyNeeds { get; set; }
+}
